8c11f1cd5f42cb618096da9a742070ef00c99f8d
Richard Lipa <rlipac31@gmail.com>
2025-07-09 14:25:52 -0500

    pi primer commit

---
+++ src/main/java/richard/lipa/Api_ForoHub/ApiForoHubApplication.java
@@ -0,0 +1,13 @@
+package richard.lipa.Api_ForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/richard/lipa/Api_ForoHub/ApiForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package richard.lipa.Api_ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
